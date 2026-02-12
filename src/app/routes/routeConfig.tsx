4aa6d8823e57b9c391f9666ac4c160f5e2bd8d44
WassimSss <tojiikan@gmail.com>
2026-02-12 10:43:49 +0100

    fixes

--- src/app/routes/routeConfig.tsx
+++ src/app/routes/routeConfig.tsx
@@ -1,10 +1,10 @@
 import type { RouteObject } from 'react-router-dom';
 import { ROUTES } from './metaRoutes';
 import { ProtectedRoute } from './ProtectedRoute';
-import { AppLayout } from '@/layouts/AppLayout';
-import { AuthLayout } from '@/layouts/AuthLayout';
-import { PAGES } from '@/pages/pages';
-import { HomeLayout } from '@/layouts/HomeLayout';
+import { AuthLayout } from '@/ui/components/layouts/AuthLayout';
+import { HomeLayout } from '@/ui/components/layouts/HomeLayout';
+import { AppLayout } from '@/ui/components/layouts/AppLayout';
+import { PAGES } from '@/ui/routing/pages';
 
 export const routes: RouteObject[] = Object.values(ROUTES).map((route) => {
 
