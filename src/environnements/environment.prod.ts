0851d3088f37053daf2686a1ee187e0b26f4f66d
Charon <Charon.odessa@proton.me>
2026-04-17 14:34:07 +0200

    remove environnement.prod.ts

--- src/environnements/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: (() => {
-    const hostname = window.location.hostname; // ex: z.stock-s.fr
-    const tenant = hostname.split('.')[0];     // ex: minette
-    return `https://${tenant}-api.stock-s.fr`;
-  })()
-};
